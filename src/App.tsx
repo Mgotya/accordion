d1410a883fd20ab3f67a5257bdadc607a2ca92f3
Michael Gotfrid <michaelg@yad2.co.il>
2021-05-12 21:11:59 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { singleReducer } from "./accordion/reducers";
+import { preventCloseReducer } from "./accordion/reducers";
 import { SampleAccordion } from "./sample-component";
 
 import "./styles.css";
 
 function App() {
-  return <SampleAccordion reducers={[singleReducer]} />;
+  return <SampleAccordion reducers={[preventCloseReducer]} />;
 }
 
 export default App;
